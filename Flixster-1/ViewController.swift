96e2a495460946d2a1ccdefad45b9fb7db864b5e
Rishabh Tewari <rishabhtewari1@gmail.com>
2021-07-07 19:52:57 -0500

    Initial Commit

---
+++ Flixster-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flixster-1
+//
+//  Created by Rishabh Tewari on 7/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
